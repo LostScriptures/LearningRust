f12d7cae93105e12fa9e62ec57dc1505c679b170
Lost Script <97259974+LostScriptures@users.noreply.github.com>
2025-08-27 06:02:10 +0000

    Played around with strings (made a pyramid)

--- crates/random-tests/src/main.rs
+++ crates/random-tests/src/main.rs
@@ -1,27 +1,13 @@
-pub mod test;
-use crate::test::A;
+use std::vec;
 
 fn main() {
-    // Tests
-    let mut s = String::from("Hello, world!");
-    let rs = &s;
+    pyramid(20);
+}
 
-    println!("{rs}");
-
-    {
-        let ms = &mut s;
-
-        ms.push_str(" it's me :)");
+fn pyramid(n: usize) {
+    for i in 0..n {
+        let spaces = " ".repeat(n - i - 1);
+        let stars = "*".repeat(2 * i + 1);
+        println!("{}{}", spaces, stars);
     }
-
-    let ss = &s;
-    println!("{ss}");
-
-    let mut a: A = A { x: 12, y: 13 };
-
-    println!("B: {} | {}", a.x, a.y);
-
-    a = a.add(10, 10);
-
-    println!("B: {} | {}", a.x, a.y);
 }
