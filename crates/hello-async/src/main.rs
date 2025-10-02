de882c934e386d0a324a85be8f8ab84691ab0dba
LostScriptures <lost.player.game@gmail.com>
2025-10-02 11:16:05 +0200

    joining on tasks

--- crates/hello-async/src/main.rs
+++ crates/hello-async/src/main.rs
@@ -1,22 +1,79 @@
-use trpl::Html;
-async fn page_title(url: &str) -> Option<String> {
+use std::time::Duration;
+use trpl::{Either, Html};
+
+#[allow(dead_code)]
+async fn page_title(url: &str) -> (&str, Option<String>) {
     let response_text = trpl::get(url).await.text().await;
-    Html::parse(&response_text)
+    let title = Html::parse(&response_text)
         .select_first("title")
-        .map(|title| title.inner_html())
+        .map(|title| title.inner_html());
+    (url, title)
 }
 
-fn main() {
+#[allow(dead_code)]
+fn webscraper() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+
+        match maybe_title {
             Some(title) => {
                 let title = title.trim();
-                println!("Title: \"{title}\" for {url}");
+                println!("Page Title: {title}");
             }
-            None => println!("{url} had no title"),
+            None => println!("Had no title"),
         }
-    })
+    });
+}
+
+#[allow(dead_code)]
+fn spawn_tasks() {
+    trpl::run(async {
+        let h1 = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("Hi {i} from task 1");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("Hi {i} from task 2");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        h1.await.unwrap();
+    });
+}
+
+fn task_join() {
+    trpl::run(async {
+        let fut1 = async {
+            for i in 1..10 {
+                println!("{i} from task 1");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let fut2 = async {
+            for i in 1..5 {
+                println!("{i} from task 2");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        trpl::join(fut1, fut2).await;
+    });
+}
+
+fn main() {
+    task_join();
 }
