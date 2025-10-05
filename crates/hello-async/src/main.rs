a6cbb046b06abbee3aed0b269c95542bee0778ef
LostScriptures <lost.player.game@gmail.com>
2025-10-05 17:26:04 +0200

    Finally finished the thread, async and streams chapter °w°

--- crates/hello-async/src/main.rs
+++ crates/hello-async/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     thread,
     time::Duration,
 };
-use trpl::{Either, Html};
+use trpl::{Either, Html, ReceiverStream, Stream, StreamExt};
 
 #[allow(dead_code)]
 async fn page_title(url: &str) -> (&str, Option<String>) {
@@ -198,6 +198,75 @@ fn custom_timeout() {
     })
 }
 
+#[allow(dead_code)]
+fn stream_test() {
+    trpl::block_on(async {
+        let a = vec![1, 2, 3, 4, 5, 9, 10, 12];
+        let iter = a.iter().map(|val| val * 2);
+        let mut stream = trpl::stream_from_iter(iter);
+
+        while let Some(val) = stream.next().await {
+            println!("{val}");
+        }
+    });
+}
+
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        for (index, message) in messages.into_iter().enumerate() {
+            let time_to_sleep = if index % 2 == 0 { 100 } else { 300 };
+            trpl::sleep(Duration::from_millis(time_to_sleep)).await;
+
+            if let Err(e) = tx.send(format!("Message: '{message}'")) {
+                eprintln!("Cannot send message '{message} : {e}'");
+                break;
+            }
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
+
+fn get_intervals() -> impl Stream<Item = u32> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let mut count = 0;
+        loop {
+            trpl::sleep(Duration::from_millis(1)).await;
+            count += 1;
+            if let Err(e) = tx.send(count) {
+                eprintln!("Unable to send Interval {count} : {e}");
+            }
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
+
+#[allow(dead_code)]
+fn receiver_stream() {
+    trpl::block_on(async {
+        let messages = get_messages().timeout(Duration::from_millis(200));
+        let intervals = get_intervals()
+            .map(|count| format!("Interval: {count}"))
+            .throttle(Duration::from_millis(100))
+            .timeout(Duration::from_secs(10));
+        let merged = messages.merge(intervals).take(20);
+        let mut stream = pin!(merged);
+
+        while let Some(result) = stream.next().await {
+            match result {
+                Ok(message) => println!("{message}"),
+                Err(reason) => eprintln!("Problem: {reason:?}"),
+            }
+        }
+    })
+}
+
 fn main() {
-    custom_timeout();
+    receiver_stream();
 }
